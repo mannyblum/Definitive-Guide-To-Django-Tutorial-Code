ef5cf18da35be176f54934ca41ecafdeee16eaff
Manny Blum <manny.blum@cmgdigital.com>
2011-06-02 17:26:30 -0400

    Working on more stuff from the book

--- settings.py
+++ settings.py
@@ -11,7 +11,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
@@ -27,7 +27,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/New_York'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -106,6 +106,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/Users/mblum/Sites/djangotut/mysite/templates',
 )
 
 INSTALLED_APPS = (
--- views.py
+++ views.py
@@ -1,19 +1,17 @@
-from django.http import Http404, HttpResponse
+from django.shortcuts import render_to_response
 import datetime
 
 def hello(request):
     return HttpResponse("Hello World")
 
 def current_datetime(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
+    current_date = datetime.datetime.now()
+    return render_to_response('current_datetime.html', locals())
 
 def hours_ahead(request, offset):
     try:
         offset = int(offset)
     except ValueError:
         raise Http404()
-    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    html = "<html><body>In %s hour(s), it wil be %s.</body></html>" % (offset, dt)
-    return HttpResponse(html)
+    next_time = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    return render_to_response('hours_ahead.html', locals())
